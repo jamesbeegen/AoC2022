6410fab49ab5339be4bf32e52cb80db89d8219bf
Beegen, James C (CGI Federal) <james.beegen@cgifederal.com>
2022-12-01 10:11:06 -0500

    Day 1 - Part 1 - Completed

--- src/day1.rs
+++ src/day1.rs
@@ -1,42 +1,36 @@
-fn find_largest(cal_list: Vec<&str>) -> (i32, i32) {
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// Read lines from puzzle input file
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() {
     let mut current_elf = 1;
     let mut largest_elf = 1;
     let mut largest_cals = 0;
     let mut temp_sum = 0;
 
-    for cal in cal_list {
-        if cal != "" {
-            temp_sum += cal.parse::<i32>().unwrap();
-        }
-        else {
-            if temp_sum > largest_cals {
-                largest_cals = temp_sum;
-                largest_elf = current_elf;
+    if let Ok(lines) = read_lines("./puzzle_input/day1-input.txt") {
+        for line in lines {
+            if let Ok(cal) = line{
+                if cal != "" {
+                    temp_sum += cal.parse::<i32>().unwrap();
+                }
+                else {
+                    if temp_sum > largest_cals {
+                        largest_cals = temp_sum;
+                        largest_elf = current_elf;
+                    }
+                    current_elf += 1;
+                    temp_sum = 0;
+                }
             }
-            current_elf += 1;
-            temp_sum = 0;
         }
     }
-    return (largest_elf, largest_cals);
-}
-
-fn main() {
-    let cal_list = vec![
-        "1000",
-        "2000",
-        "3000",
-        "",
-        "4000",
-        "",
-        "5000",
-        "6000",
-        "",
-        "7000",
-        "8000",
-        "9000",
-        "",
-        "10000"];
-    
-    let largest = find_largest(cal_list);
-    println!("Elf #{} is carrying the most calories: {}", largest.0, largest.1);
+    println!("Elf #{} is carrying the most calories: {}", largest_elf, largest_cals);
 }
